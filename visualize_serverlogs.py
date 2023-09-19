10779048adacffd59e3423d79eeafccd991face1
aozer <aozer@turksat.com.tr>
2023-09-19 15:02:07 +0300

    adding time series

--- visualize_serverlogs.py
+++ visualize_serverlogs.py
@@ -13,3 +13,24 @@ plt.xticks(rotation=45)
 plt.xlabel("Request Method")
 plt.ylabel("Count")
 plt.show()
+
+
+
+
+# Convert the "Timestamp" column to a datetime data type
+df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+
+# Set the "Timestamp" column as the index of the DataFrame
+df.set_index('Timestamp', inplace=True)
+
+# Resample the data to a frequency of seconds and count the number of requests per second
+requests_per_second = df.resample('S').size()
+
+# Plot the time series data
+plt.figure(figsize=(12, 6))
+requests_per_second.plot()
+plt.title("Requests Per Second Over Time")
+plt.xlabel("Time")
+plt.ylabel("Requests Count")
+plt.grid(True)
+plt.show()
